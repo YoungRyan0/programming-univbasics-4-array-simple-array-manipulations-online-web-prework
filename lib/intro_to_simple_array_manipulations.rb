375c4fb938f1301a965c7703d170e35bd4a5e405
Ryan Young <young.ryan1979@gmail.com>
2019-09-23 19:52:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -44,6 +44,11 @@ def using_flatten (array)
   array.flatten
 end
 
+def using_delete (array, string)
+  array.delete(string)
+end
+
+
 def using_delete_at (instructors, steven)
   instructors.delete_at(steven)
 end
